c727171c81c08a9d619e03bc44ed66e47a769a93
bryant <bryantkrause@gmail.com>
2020-05-02 20:00:05 -0700

    Added the monster jam

--- models/Monster.js
+++ models/Monster.js
@@ -1,8 +1,18 @@
 module.exports = (model, Schema) => {
 
     const Board = new Schema({
-      title: String,
-      description: String,
+      Name: String,
+      Alignment: String,
+      Senses: Object,
+      Stats: Object,
+      AC: Number,
+      Speed: Number,
+      HP: Number,
+      Challenge: Number,
+      XP: Number,
+      Abilities: Array,
+      Languages: Array,
+      Description: String,
       owner: { type: Schema.Types.ObjectId, ref: 'User' },
       table: [{ type: Schema.Types.ObjectId, ref: 'Table' }],
       company: [{ type: Schema.Types.ObjectId, ref: 'Company' }],
