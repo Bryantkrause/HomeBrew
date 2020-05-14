4b8f4ec59c43f14dd1c304b473396662f5deabc4
bryant <bryantkrause@gmail.com>
2020-05-13 20:05:08 -0700

    making a better table

--- client/src/components/TableDisplay/TableDisplay.js
+++ client/src/components/TableDisplay/TableDisplay.js
@@ -1,47 +1,45 @@
 import React from 'react';
-import Card from '@material-ui/core/Card';
+import {Table} from 'reactstrap'
 import TableContext from '../../utils/TableContext'
 
-const TableDisplay = () => {
+const TableDisplay = _ => {
 
-    const {
-      master,
-      description,
-      players,
-    } = React.useContext(TableContext)
-  
-    console.log(` Context states are...
-    master: ${master}
-    description: ${description}
-    `)
-  
-    return (
-  
-      <div>
-        <Card className="boardTitle">
-          <CardHeader 
-            title={master}
-            subheader={description}
-          />
-        </Card>
-        
-          {
-            players.map( table => 
-              <div>
-                <Card>
-                  <CardHeader 
-                    title={table.title}
-                  />
-                  <CardActionArea>
-  
-                  </CardActionArea>
-                </Card>
-              </div>
-              )
-          }
-        
-      </div>
-       )
+  const { players } = React.useContext(TableContext)
+
+  return (
+    <Table dark>
+    <thead>
+        <tr>
+            <th><h4>#</h4></th>
+            <th><h4>name</h4></th>
+            <th><h4>background</h4></th>
+            <th><h4>race</h4></th>
+            <th><h4>alignment</h4></th>
+            <th><h4>proficiency</h4></th>
+            <th><h4>description</h4></th>
+            <th><h4>ac</h4></th>
+        </tr>
+    </thead>
+    <tbody onLoad={getTasks}>
+    {
+        players.map( ({_id, name, background, race, alignment, proficiency, description, ac}, i) =>
+        <tr key={_id}>
+            <td>{i+1}</td>
+            <td>{name}</td>
+            <td>{background}</td>
+            <td>{race}</td>
+            <td>{alignment}</td>
+            <td>{proficiency}</td>
+            <td>{description}</td>
+            <td>{ac}</td>
+        </tr>
+        )
     }
-    
-    export default TableDisplay
+    </tbody>
+</Table>
+  )
+
+
+}
+
+export default TableDisplay
